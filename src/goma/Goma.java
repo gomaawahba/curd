7b911a5f5b97ef97f8bee81c7dde0711a8e3da41
gomaa <gomaa@gmail.com>
2023-07-01 03:27:29 -0700

    my project GUI

---
+++ src/goma/Goma.java
@@ -0,0 +1,14 @@
+
+package goma;
+
+
+public class Goma {
+
+    public static void main(String[] args) {
+     //   form f=new from();
+        //from f=new from();
+      //  from f=new from();
+        // TODO code application logic here
+    }
+    
+}
